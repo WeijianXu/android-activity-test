b2f1430116c20c970be3ac7ff6fcca578438c36d
WeijianXu <dlmu_xwj2012@163.com>
2017-07-26 15:32:23 +0800

    add more examples about how to use Intent

--- app/src/main/java/com/example/administrator/activitytest/FirstActivity.java
+++ app/src/main/java/com/example/administrator/activitytest/FirstActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -11,6 +12,8 @@ import android.widget.Button;
 import android.widget.Toast;
 
 public class FirstActivity extends AppCompatActivity {
+
+    private static final String TAG = "FirstActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,13 +25,28 @@ public class FirstActivity extends AppCompatActivity {
             public void onClick(View v) {
                 // Toast.makeText(FirstActivity.this, "You clicked button 1", Toast.LENGTH_SHORT).show();
                 // finish();    // 结束活动，返回主界面，相当于按 home 键
+
                 // Intent intent = new Intent(FirstActivity.this, SecondActivity.class); // （显式Intent）启动第二个活动
+
                 /*Intent intent = new Intent("com.example.administrator.activitytest.ACTION_START"); // （隐式Intent）启动指定活动，活动注册在Manifest中
                 intent.addCategory("com.example.administrator.activitytest.MY_CATEGORY");*/
-                Intent intent = new Intent(Intent.ACTION_VIEW);
-                intent.setData(Uri.parse("http://www.baidu.com"));
 
-                startActivity(intent);
+                /*Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("http://www.baidu.com"));*/
+
+                // 调用系统打电话功能
+                /*Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:10086"));*/
+
+                // 传递数据
+                /*String data = "Hello SecondActivity";
+                Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+                intent.putExtra("extra_data", data);*/
+
+                Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+
+                // startActivity(intent);
+                startActivityForResult(intent, 1);
             }
         });
     }
@@ -51,5 +69,18 @@ public class FirstActivity extends AppCompatActivity {
             default:
         }
         return true;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case 1:
+                if (resultCode == RESULT_OK) {
+                    String returnedData = data.getStringExtra("data_return");
+                    Log.d(TAG, "onActivityResult: " + returnedData);
+                }
+                break;
+            default:
+        }
     }
 }
--- app/src/main/java/com/example/administrator/activitytest/SecondActivity.java
+++ app/src/main/java/com/example/administrator/activitytest/SecondActivity.java
@@ -1,7 +1,11 @@
 package com.example.administrator.activitytest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -9,5 +13,34 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second_layout);
+
+        // 获取数据
+        /*Intent intent = getIntent();
+        String data = intent.getStringExtra("extra_data");
+        Log.d("SecondActivity", data);*/
+
+        Button button2 = (Button) findViewById(R.id.button_2);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // (new SecondActivity()).button2ClickCB(new Intent()); // 使用这种方式，数据并不能被 FirstActivity 获取，尽管不会报错
+                Intent intent = new Intent();
+                intent.putExtra("data_return", "hello FirstActivity");
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        });
+    }
+
+    protected void button2ClickCB(Intent intent) {
+        intent.putExtra("data_return", "hello FirstActivity");
+        setResult(RESULT_OK, intent);
+    }
+
+    @Override
+    public void onBackPressed() {
+        this.button2ClickCB(new Intent());
+        finish();
     }
 }
